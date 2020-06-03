1eeae88c87c5fc0a34021e10d6bb593bc909a355
Bill <william.hine2019@gmail.com>
2020-06-03 18:13:23 -0400

    Added EditorState class and MementoPattern class

---
+++ MoshDesignPatterns/EditorState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoshDesignPatterns
+{
+    class EditorState
+    {
+        private string content;
+        public string Content { get => content; set => content = value; }
+       
+        public EditorState(String content)
+        {
+            this.content = content;
+        }
+    }
+}
